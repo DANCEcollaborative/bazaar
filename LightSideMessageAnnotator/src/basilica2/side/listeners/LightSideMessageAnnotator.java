22fa1dda4a5bc696aa52be15dbd326693c786742
Chas Murray <rcmurray@andrew.cmu.edu>
2020-04-03 14:29:00 -0400

    Checkpoint communication with LightSide

--- LightSideMessageAnnotator/src/basilica2/side/listeners/LightSideMessageAnnotator.java
+++ LightSideMessageAnnotator/src/basilica2/side/listeners/LightSideMessageAnnotator.java
@@ -21,9 +21,13 @@ public class LightSideMessageAnnotator extends BasilicaAdapter
 {
 	//String pathToLightSide = "/Users/researcher/Downloads/LightSide_2.3.1_20141107";
 	//String pathToModel = "saved/test.xml";
-	String pathToLightSide = "../../LightSideMessageAnnotator/runtime/LightSide_2.3.1_20141107";
-	String pathToModel = "saved/gst_reasoning_model.model.xml";
-	String predictionCommand = "scripts/predict.sh";
+	// String pathToLightSide = "../../LightSideMessageAnnotator/runtime/LightSide_2.3.1_20141107";
+	String pathToLightSide = "/Users/rcmurray/git/DANCEcollaborative/bazaar/lightside/"; 
+	// String pathToLightSide = "/Users/rcmurray/git/DANCEcollaborative/bazaar/LightSideMessageAnnotator/runtime/LightSide_2.3.1_20141107";
+	// String pathToModel = "saved/gst_reasoning_model.model.xml";
+	String pathToModel = "/Users/rcmurray/git/DANCEcollaborative/bazaar/lightside/models/negative.simple.model.xml";
+	String predictionCommand = "/Users/rcmurray/git/DANCEcollaborative/bazaar/lightside/scripts/predict.sh";
+	// String predictionCommand = "scripts/test.sh";
 	
 	OutputStream stdin;
 	InputStream stderr;
@@ -39,27 +43,88 @@ public class LightSideMessageAnnotator extends BasilicaAdapter
 		super(a);
 		
 		pathToLightSide = getProperties().getProperty("pathToLightSide", pathToLightSide);
+		System.err.println("pathToLightSide: " + pathToLightSide);
 		pathToModel = getProperties().getProperty("pathToModel", pathToModel);
+		System.err.println("pathToModel: " + pathToModel);
 		predictionCommand = getProperties().getProperty("predictionCommand", predictionCommand);
+		System.err.println("predictionCommand: " + predictionCommand);
 		
 		try
 		{
-			File lightSideLocation = new File(pathToLightSide);
-			process = Runtime.getRuntime().exec(new String[] { predictionCommand, pathToModel }, null, lightSideLocation);
-
-		}
-
-		catch (IOException e)
+			// File lightSideLocation = new File(pathToLightSide);
+			// process = Runtime.getRuntime().exec(new String[] { predictionCommand, pathToModel }, null, lightSideLocation); // ORIG
+			// process = Runtime.getRuntime().exec(predictionCommand, null, lightSideLocation); 
+			// process=Runtime.getRuntime().exec(predictionCommand);
+			
+			/// test replacement for "process = ..." line above /// >>> getting Instantiation exception
+			try {
+				// process=Runtime.getRuntime().exec(predictionCommand);
+				ProcessBuilder pb = new ProcessBuilder(predictionCommand);
+				pb.inheritIO(); 
+				process = pb.start();
+				// process.waitFor();
+				
+			} 
+			catch (Exception e)
+			{
+				System.err.println("LightSideMessageAnnotator, process creation error - place 1");
+				e.printStackTrace();
+			}
+			// Process process = pb.start();
+			///////////////////////////////////////////////////////
+			
+			/// test replacement for "process = ..." line above /// >>> getting Instantiation exception
+			// ProcessBuilder pb = new ProcessBuilder(predictionCommand, pathToModel);
+			// pb.directory(lightSideLocation);
+			// What to do for the "null" in the original command? 
+			// Process process = pb.start();
+			///////////////////////////////////////////////////////
+			
+			/// test replacement for "process = ..." line above /// 
+			// ProcessBuilder pb = new ProcessBuilder();
+			// pb.command(predictionCommand);
+			// pb.command(new String[] {predictionCommand,pathToModel});
+			// pb.directory(lightSideLocation);
+			// What to do for the "null" in the original command? 
+			// Process process = pb.start();
+			///////////////////////////////////////////////////////
+			
+			/// test replacement for "process = ..." line above /// 
+			// ProcessBuilder pb = new ProcessBuilder(predictionCommand);
+			// Process process = pb.start();
+			///////////////////////////////////////////////////////
+			
+			Boolean isAlive = process.isAlive();
+			if (isAlive) {
+				System.err.println("LightSide process is alive");
+			}
+			else {
+				System.err.println("LightSide process is NOT alive");			
+			}
+		} 
+		catch (Exception e)
 		{
+			System.err.println("LightSideMessageAnnotator, process creation error - place 2");
 			e.printStackTrace();
 		}
 
-		stdin = process.getOutputStream();
-		stderr = process.getErrorStream();
-		stdout = process.getInputStream();
+
+		// stdin = process.getOutputStream();
+		// stderr = process.getErrorStream();
+		// stdout = process.getInputStream();
 
 		reader = new BufferedReader(new InputStreamReader(stdout));
 		writer = new BufferedWriter(new OutputStreamWriter(stdin));
+		
+		
+		
+		try {
+			Boolean readerStatus = reader.ready();
+			System.err.println("reader ready");
+		}
+		catch (IOException e) {
+			System.err.println("reader NOT ready");
+		}
 
 	}
 
@@ -98,13 +163,54 @@ public class LightSideMessageAnnotator extends BasilicaAdapter
 
 		if(process != null) try
 		{
-			writer.write(text + "\n");
-			writer.flush();
-
+			
+			// ============== UPDATE ATTEMPT ==============
+			try { 
+				// System.err.println("LightSideMessageAnnotator, write text: " + text);  // TEMP
+				// writer.write(text + "\n");
+				writer.write(text);
+				writer.newLine();
+				// writer.flush();
+			}
+			catch (Exception e)
+			{
+				// TODO Auto-generated catch block
+				System.err.println("LightSideMessageAnnotator, write/newline error");
+				e.printStackTrace();
+			}
+			
+			try {
+				String line = reader.readLine();
+				if (line == null)
+				{
+					System.err.println("LightSideMessageAnnotator, read line: NULL");
+				}	
+				else {
+					String[] response = line.split("\\s+");
+					label = response[0];	
+				}
+			}
+			catch (Exception e)
+			{
+				// TODO Auto-generated catch block
+				System.err.println("LightSideMessageAnnotator, read/label error");
+				e.printStackTrace();
+			}
+			// ============== UPDATE ATTEMPT ==============
+			
+			
+			/** ================= ORIG ==============
+			// System.err.println("LightSideMessageAnnotator, write text: " + text);  // TEMP
+			// writer.write(text + "\n");
+			writer.write(text);
+			writer.newLine();
+			// writer.flush();
+			 
 			String line = reader.readLine();
+			//  System.err.println("line = reader.readline(): " + line);        // TEMP
 			if (line != null)
 			{
-				System.out.println(line);
+				System.err.println("LightSideMessageAnnotator, read line: " + line);
 				String[] response = line.split("\\s+");
 				label = response[0];
 			}
@@ -112,11 +218,13 @@ public class LightSideMessageAnnotator extends BasilicaAdapter
 			{
 				System.err.println("response from LightSide is null!");
 			}
+			*/ // ================= ORIG ==============
 
 		}
 		catch (Exception e)
 		{
 			// TODO Auto-generated catch block
+			System.err.println("LightSideMessageAnnotator: Null process");
 			e.printStackTrace();
 		}
 		return label;
