e3277af30d3b2bcfdef89ca645e7d6e30803e538
Chas Murray <rcmurray@andrew.cmu.edu>
2020-04-08 18:29:27 -0400

    LightSide interface: partial cleanup

--- LightSideMessageAnnotator/src/basilica2/side/util/MultipartUtility.java
+++ LightSideMessageAnnotator/src/basilica2/side/util/MultipartUtility.java
@@ -54,7 +54,6 @@ public class MultipartUtility {
         httpConn.setRequestProperty("Content-Type",
                 "multipart/form-data; boundary=" + boundary);
         httpConn.setRequestProperty("User-Agent", "CodeJava Agent");
-        //httpConn.setRequestProperty("Test", "Bonjour");
         outputStream = httpConn.getOutputStream();
         writer = new PrintWriter(new OutputStreamWriter(outputStream, charset),
                 true);
