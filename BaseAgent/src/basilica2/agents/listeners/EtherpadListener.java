af7a9e9ad932374eb7d083081c54fbb5a6d41cb0
rcmurray <>
2021-05-10 12:01:34 -0400

    Cleanup for EtherpadListener

--- BaseAgent/src/basilica2/agents/listeners/EtherpadListener.java
+++ BaseAgent/src/basilica2/agents/listeners/EtherpadListener.java
@@ -8,9 +8,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import javax.swing.JOptionPane;
-
 import basilica2.agents.components.InputCoordinator;
 import basilica2.agents.events.EchoEvent;
 import basilica2.agents.events.FileEvent;
@@ -19,21 +16,16 @@ import basilica2.agents.events.PresenceEvent;
 import basilica2.agents.events.ReadyEvent;
 import basilica2.agents.events.TypingEvent;
 import basilica2.agents.events.WhiteboardEvent;
-
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-
 import de.fhg.ipsi.utils.StringUtilities;
 import edu.cmu.cs.lti.basilica2.core.Agent;
 import edu.cmu.cs.lti.basilica2.core.Component;
 import edu.cmu.cs.lti.basilica2.core.Event;
 import edu.cmu.cs.lti.project911.utils.log.Logger;
 
-//TODO: EtherpadListener.properties
 
 public class EtherpadListener extends BasilicaAdapter
 {	
-	
-	//TODO: parameterize these.
 	private int port = 3306;
 	// int checkInterval = 2000;
 	private String host = "128.2.220.51";	
@@ -42,44 +34,13 @@ public class EtherpadListener extends BasilicaAdapter
 	private String databaseName = "etherpad-lite";
 	private String tableName = "store";
 	private String roomNamePrefix = "j";
-	// String agentUserName="guest";
 	MysqlDataSource dataSource = new MysqlDataSource();
 	Connection conn;	
 	private boolean connected = false;	
-	// int chatID;
-	// int userID;
-	// long lastTime = 0;
 	private InputCoordinator source;
 	private String agentID; 
-	// private String status = "";
 	private String roomName; 	
 	private int rowCount; 	
-	
-	/* (non-Javadoc)
-	 * @see basilica2.agents.components.ChatClient#disconnect()
-	 */
-	/*
-	@Override
-	public void disconnect()
-	{
-		System.out.println("Etherpad disconnecting...");
-		connected = false;
-		try
-		{
-			exitChat(conn);
-			conn.close();
-		}
-		catch (SQLException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	*/ 
-
-	//TODO: poll for incoming messages
-	//TODO: report presence events
-	//TODO: report extant students at login
 	
 
 	public EtherpadListener(Agent a)
@@ -141,24 +102,6 @@ public class EtherpadListener extends BasilicaAdapter
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
-		/**
-		if(e instanceof MessageEvent)
-		{
-			MessageEvent me = (MessageEvent) e;
-			try
-			{
-				insertMessage(me.getText(), conn);
-			}
-			catch (Exception e1)
-			{
-				System.err.println("couldn't send message: "+me);
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		}
-		//TODO: private messages? "beeps"?
-		*/ 
 	}
 
 	protected long getSystemTime() throws SQLException
@@ -192,57 +135,6 @@ public class EtherpadListener extends BasilicaAdapter
 		    if (rowCount > 1) {
 		    	System.err.println("=== Activity detected in Etherpad *** " + roomNamePrefix + " ***");
 		    }
-		    /**
-			while (rs.next())
-			{
-				
-				lastTime = rs.getLong("timestamp");
-				
-				Date when = new Date(lastTime);
-				String text = rs.getString("message");
-				String userFrom = rs.getString("firstname")+" "+rs.getString("lastname");
-//				String userFrom = rs.getString("username");
-				String incomingUsername = rs.getString("username");
-				boolean systemEvent = rs.getInt("system") != 0;
-				
-				Event bazaarEvent = null;
-
-				this.log(Logger.LOG_NORMAL, "Incoming Etherpad Event "+when+"\t"+userFrom+"\t'"+text+"'");
-				
-				if(incomingUsername.equals(agentUserName))
-				{
-					if(!systemEvent)
-					{
-						bazaarEvent = new EchoEvent(this, new MessageEvent(this, this.getAgent().getUsername(), text));
-					}
-				}
-				
-				else if(systemEvent)
-				{
-					if(text.equals("enter"))
-					{
-						bazaarEvent = new PresenceEvent(this, userFrom, PresenceEvent.PRESENT);
-					}
-					if(text.equals("exit"))
-					{
-						bazaarEvent = new PresenceEvent(this, userFrom, PresenceEvent.ABSENT);
-					}
-					else this.log(Logger.LOG_WARNING, "Unknown System event '"+text+"' from "+userFrom);
-				}
-				
-				else
-				{
-					bazaarEvent = new MessageEvent(this, userFrom, text);
-				}
-				
-				
-				if(bazaarEvent != null)
-				{
-					this.broadcast(bazaarEvent);
-				}
-
-			}
-			*/ 
 			rs.close();
 			stmt.close();
 			
