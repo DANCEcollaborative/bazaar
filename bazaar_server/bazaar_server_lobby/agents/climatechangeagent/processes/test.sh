4a12bfbf8d01b1454f74082098a7f3cfc7940d35
Chas Murray <rcmurray@forum.lti.cs.cmu.edu>
2021-07-08 17:08:21 -0400

    Share Docker & host agent directories; tweak to climatechangeagent script

--- bazaar_server/bazaar_server_lobby/agents/climatechangeagent/processes/test.sh
+++ bazaar_server/bazaar_server_lobby/agents/climatechangeagent/processes/test.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-echo "script started"
 cd processes
-mkdir crazy_dir
 chmod 777 /mysql-files
 mysql -u root -psmoot -h nodechat  -P 3306 <<MYSQL_QUERY
 USE nodechat
 SELECT 'type', 'username', 'useraddress', 'userid', 'timestamp', 'roomname' , 'content' UNION ALL SELECT type,username,useraddress,userid,timestamp,name as roomname,replace(content,'"',"'") as content FROM message JOIN room ON  room.id = message.roomid INTO OUTFILE '/mysql-files/bazaar_2021-07-08a.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 MYSQL_QUERY
 python3 chat_logs.py /mysql-files/bazaar_2021-07-08a.csv jeopardy 06/04/21 00 07/08/21 23 >> python_output.txt
-mkdir crazier_dir
-echo "script ended"
