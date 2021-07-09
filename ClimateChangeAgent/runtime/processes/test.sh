25bd546701163f1044fedc7b42c177f870258ca5
rcmurray <>
2021-07-09 15:46:34 -0400

    Use general python3 process for ClimateChangeAgent script

--- ClimateChangeAgent/runtime/processes/test.sh
+++ ClimateChangeAgent/runtime/processes/test.sh
@@ -2,5 +2,5 @@
 AGENT=$1
 cd processes
 mkdir crazy_dir
-/Users/rcmurray/.pyenv/shims/python3 chat_logs.py bazaar_2021-06-11-am.csv $AGENT 06/04/21 00 07/08/21 23 >> python_output.txt
+python3 chat_logs.py bazaar_2021-06-11-am.csv $AGENT 06/04/21 00 07/08/21 23 >> python_output.txt
 mkdir crazier_dir
