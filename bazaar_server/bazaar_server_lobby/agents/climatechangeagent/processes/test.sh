72e2e0e92e757e144dbcb0043b981f394fbb381d
rcmurray <>
2021-07-12 18:24:13 -0400

    Add dedicated chat log handler

--- bazaar_server/bazaar_server_lobby/agents/climatechangeagent/processes/test.sh
+++ bazaar_server/bazaar_server_lobby/agents/climatechangeagent/processes/test.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-ROOM_PREFIX=$1
+AGENT=$1
 cd processes
-chmod 777 /mysql-files
-mysql -u root -psmoot -h nodechat  -P 3306 <<MYSQL_QUERY
-USE nodechat
-SELECT 'type', 'username', 'useraddress', 'userid', 'timestamp', 'roomname', 'content' UNION ALL SELECT typ
-e,username,useraddress,userid,timestamp,'$ROOM_PREFIX' AS roomname,REPLACE(content,'"',"'") AS content FROM
- message JOIN (SELECT id FROM room WHERE name = '$ROOM_PREFIX') subquery ON subquery.id = message.roomid IN
-TO OUTFILE '/mysql-files/$ROOM_PREFIX.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-MYSQL_QUERY
-python3 chat_logs.py /mysql-files/$ROOM_PREFIX.csv $ROOM_PREFIX
-rm $ROOM_PREFIX.csv
-mv *.csv ../chat_logs
+mkdir crazy_dir
+python3 chat_logs.py bazaar_2021-06-11-am.csv $AGENT 06/04/21 00 07/08/21 23 >> python_output.txt
+mkdir crazier_dir
