aa7cd5677cf2f71dfb5d7dfc883ceda538036544
rcmurray <>
2021-10-19 11:16:26 -0400

    Add file_gated step type; tweaks to DiamondKataPSIAMQAgent

---
+++ BaseAgent/src/basilica2/agents/listeners/FileGatekeeper.java
@@ -0,0 +1,83 @@
+package basilica2.agents.listeners;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import basilica2.agents.components.InputCoordinator;
+import basilica2.agents.components.StateMemory;
+import basilica2.agents.data.PromptTable;
+import basilica2.agents.events.MessageEvent;
+import basilica2.agents.events.FileEvent;
+import basilica2.agents.events.StepDoneEvent;
+import basilica2.agents.listeners.plan.Step; 
+import edu.cmu.cs.lti.basilica2.core.Agent;
+import edu.cmu.cs.lti.basilica2.core.Event;
+import edu.cmu.cs.lti.project911.utils.log.Logger;
+
+public class FileGatekeeper extends BasilicaAdapter
+{
+	private Step step = null; 
+	private String stepName = "step";
+	private PromptTable prompter;
+	private String fileName; 
+
+	public void setStepName(String stepName)
+	{
+		this.stepName = stepName; 
+	}
+	
+	public void setFileName(String fileName) {
+		this.fileName = fileName; 
+	}
+
+	public FileGatekeeper(Agent a)
+	{
+		super(a);
+		prompter = new PromptTable("plans/gatekeeper_prompts.xml");
+
+//		if(step.attributes.containsKey("file"))
+//		{
+//			fileName = step.attributes.get("file");
+//		}
+	}
+	
+
+	// TODO: Files created early and/or out of order
+	@Override
+	public void processEvent(InputCoordinator source, Event event)
+	{
+		if (event instanceof FileEvent)
+		{
+			System.err.println("FileGateKeeper: FileEvent received"); 
+			FileEvent fileEvent = (FileEvent)event; 
+			String fileName = fileEvent.getFileName();
+			System.err.println("FileGateKeeper: FileEvent fileName: " + fileName); 
+			if (fileName.equals(fileName)) {
+				source.pushEventProposal(new MessageEvent(source, getAgent().getUsername(), prompter.lookup("FILE_STEP_COMPLETE"), "FILE_STEP_COMPLETE"), 1.0, 2);
+				source.pushEvent(new StepDoneEvent(source, stepName));
+			}
+		}
+	}
+
+	@Override
+	public Class[] getListenerEventClasses()
+	{
+		return new Class[] { FileEvent.class };
+	}
+
+	@Override
+	public void preProcessEvent(InputCoordinator source, Event event)
+	{
+	}
+
+	
+	@Override
+	public Class[] getPreprocessorEventClasses()
+	{
+		return null;
+	}
+}
---
+++ BaseAgent/src/basilica2/agents/listeners/plan/FileStepHandler.java
@@ -0,0 +1,66 @@
+package basilica2.agents.listeners.plan;
+
+import basilica2.agents.components.InputCoordinator;
+import basilica2.agents.components.OutputCoordinator;
+import basilica2.agents.data.PromptTable;
+import basilica2.agents.events.MessageEvent;
+import basilica2.agents.listeners.FileGatekeeper;
+import basilica2.util.TimeoutAdapter;
+import edu.cmu.cs.lti.basilica2.core.Agent;
+import edu.cmu.cs.lti.project911.utils.log.Logger;
+import edu.cmu.cs.lti.project911.utils.time.Timer;
+
+public class FileStepHandler implements StepHandler
+{
+
+	private FileGatekeeper filegatekeeper = null;
+	private PromptTable prompter = null;
+	
+	@Override
+	public void execute(final Step currentStep, final PlanExecutor overmind, final InputCoordinator source)
+	{
+		Logger.commonLog(getClass().getSimpleName(),Logger.LOG_NORMAL,"starting file gated step...");
+		if(filegatekeeper == null)
+		{
+			Agent a = overmind.getAgent();
+			filegatekeeper = new FileGatekeeper(a);
+			prompter = new PromptTable("plans/gatekeeper_prompts.xml");
+		}
+
+		filegatekeeper.setStepName(currentStep.name);	
+		String fileName = currentStep.attributes.get("file");
+		filegatekeeper.setFileName(fileName); 
+		
+		overmind.addHelper(filegatekeeper);
+
+		
+		String checkinPrompt = currentStep.attributes.get("checkin_prompt");
+		if(checkinPrompt == null)
+		{
+			checkinPrompt = "WAIT_FOR_CHECKIN";
+		}
+		
+		if(!checkinPrompt.equals("NONE"))
+		{
+			source.pushEventProposal(new MessageEvent(source, overmind.getAgent().getUsername(), prompter.lookup(checkinPrompt), "WAIT_FOR_CHECKIN"), OutputCoordinator.LOW_PRIORITY, 10);
+		}
+	
+//		String type = "prompt";
+//		currentStep.executeStepHandlers(source, type);
+		
+		new Timer(Math.max(currentStep.timeout - 180, currentStep.timeout*0.75), currentStep.name, new TimeoutAdapter()
+		{
+			@Override
+			public void timedOut(String id)
+			{
+				if(currentStep.equals(overmind.currentPlan.currentStage.currentStep)) //the plan has not progressed on its own yet
+				{
+					MessageEvent softWarning = new MessageEvent(source, overmind.getAgent().getUsername(), prompter.lookup("FILE_STEP_TIMEOUT_WARNING"), "FILE_STEP_TIMEOUT_WARNING");
+					source.pushEventProposal(softWarning, OutputCoordinator.LOW_PRIORITY, 20);
+				}
+			}
+		}).start();
+		
+	}
+	
+}
