f466c2c4b7f368bd3cd7081279247c0a8630043f
qjiang002 <jiangqi@jiangs-mbp.myfiosgateway.com>
2022-04-30 16:50:13 -0400

    remove ClimateChangeRLAgent

--- ClimateChangeRLAgent/src/basilica2/myagent/listeners/plan/StepHandler.java
+++
@@ -1,8 +0,0 @@
-package basilica2.myagent.listeners.plan;
-
-import basilica2.agents.components.InputCoordinator;
-
-public interface StepHandler
-{
-	public void execute(Step currentStep, PlanExecutor overmind, InputCoordinator source);
-}
