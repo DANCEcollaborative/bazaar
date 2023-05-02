8b6716fb47669808877ace1e60367b12a7db5f41
rcmurray <>
2023-05-02 15:27:53 -0400

    KeywordStepHandler working

--- BaseAgent/src/basilica2/agents/listeners/KeywordGatekeeper.java
+++ BaseAgent/src/basilica2/agents/listeners/KeywordGatekeeper.java
@@ -15,6 +15,7 @@ import basilica2.agents.events.MessageEvent;
 import basilica2.agents.events.FileEvent;
 import basilica2.agents.events.StepDoneEvent;
 import basilica2.agents.events.priority.PriorityEvent;
+import basilica2.agents.listeners.KeywordWatcher;
 import basilica2.agents.listeners.plan.Step; 
 import edu.cmu.cs.lti.basilica2.core.Agent;
 import edu.cmu.cs.lti.basilica2.core.Event;
@@ -25,7 +26,7 @@ public class KeywordGatekeeper extends BasilicaAdapter
 	private Step step = null; 
 	private String stepName = "step";
 	private PromptTable prompter;
-	private String fileName; 
+	private KeywordWatcher keywordWatcher = null;
 
 	public void setStepName(String stepName)
 	{
@@ -36,36 +37,18 @@ public class KeywordGatekeeper extends BasilicaAdapter
 	{
 		super(a);
 		prompter = new PromptTable("plans/gatekeeper_prompts.xml");
-//		keywordWatcher = 										// TO-DO: Try to get existing KeywordWatcher (assuming it exists)
-
-//		if(step.attributes.containsKey("file"))
-//		{
-//			fileName = step.attributes.get("file");
-//		}
 	}
 	
 
 	@Override
 	public void processEvent(InputCoordinator source, Event event)
 	{
-		if (event instanceof FileEvent)
+		if (event instanceof MessageEvent)
 		{
-//			System.err.println("KeywordGateKeeper.processEvent: FileEvent received"); 
-//			log(Logger.LOG_NORMAL, "KeywordGateKeeper.processEvent: FileEvent received");
-			FileEvent fileEvent = (FileEvent)event; 
-			String eventFileName = fileEvent.getFileName();
-//			log(Logger.LOG_NORMAL, "KeywordGateKeeper.processEvent: eventFileName = " + eventFileName);
-//			log(Logger.LOG_NORMAL, "KeywordGateKeeper.processEvent: fileName = " + fileName);
-//			System.err.println("KeywordGateKeeper.processEvent: eventFileName = " + eventFileName);
-//			System.err.println("KeywordGateKeeper.processEvent: FileEvent fileName: " + fileName); 
-			if (eventFileName.equals(fileName)) {
-//				log(Logger.LOG_NORMAL, "KeywordGateKeeper.processEvent: pushing FILE_STEP_COMPLETE prompt and StepDoneEvent");
-//				System.err.println("KeywordGateKeeper.processEvent: pushing FILE_STEP_COMPLETE prompt and StepDoneEvent");
-				MessageEvent stepCompleteMessageEvent = new MessageEvent(source, getAgent().getUsername(), prompter.lookup("FILE_STEP_COMPLETE"), "FILE_STEP_COMPLETE"); 
-//				source.pushProposal(PriorityEvent.makeBlackoutEvent("macro", "PromptStep", stepCompleteMessageEvent, OutputCoordinator.HIGHEST_PRIORITY, 10, 7)); 
-				source.pushNamedEventProposal("macro",stepCompleteMessageEvent, "File step complete message", 1.0, 10);
-//				log(Logger.LOG_NORMAL, "KeywordGateKeeper.processEvent, pushing StepDoneEvent  -  source:" + source.toString() + "  stepName:" + stepName);
-//				System.err.println("KeywordGateKeeper.processEvent, pushing StepDoneEvent  -  source:" + source.toString() + "  stepName:" + stepName); 
+			keywordWatcher = (KeywordWatcher)source.getPreProcessor("KeywordWatcher");
+			if (keywordWatcher.getPromptableNumEntries() == 0) {
+				MessageEvent stepCompleteMessageEvent = new MessageEvent(source, getAgent().getUsername(), prompter.lookup("KEYWORD_STEP_COMPLETE"), "KEYWORD_STEP_COMPLETE"); 
+				source.pushNamedEventProposal("macro",stepCompleteMessageEvent, "Keyword step complete message", 1.0, 10);
 				source.pushEvent(new StepDoneEvent(source, stepName));
 			}
 		}
